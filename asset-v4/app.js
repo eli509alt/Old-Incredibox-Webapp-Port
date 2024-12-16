c96dbbe40e5f69c67f3f7ccaaacdefefaa5ff07a
RizsimStudios <61947503+RizsimStudiosOfficial@users.noreply.github.com>
2024-12-15 16:52:48 -0800

    fix the loading colors

--- asset-v4/app.js
+++ asset-v4/app.js
@@ -2,83 +2,83 @@
 var app = new function() {
     this.name = "Axe Boat", this.version = "4", this.date = "2014", this.folder = "asset-v4/", this.looptime = 8e3, this.bpm = 120, this.totalframe = 384, this.nbpolo = 7, this.nbloopbonus = 1, this.bonusloopA = !0, this.bonusendloopA = !1, this.recmaxloop = 23, this.recminloop = 3, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#211101", this.col0 = "#D8A365", this.col1 = "#BA7B3D", this.col2 = "#A16224", this.col3 = "#6A3501", this.col4 = "#422101", this.animearray = [{
         name: "drum1_kick",
-        color: "E5A90E",
+        color: "ffc075",
         uniqsnd: !1
     }, {
         name: "drum2_snare",
-        color: "E5A90E",
+        color: "ffc075",
         uniqsnd: !1
     }, {
         name: "drum3_touti",
-        color: "E5A90E",
+        color: "ffc075",
         uniqsnd: !0
     }, {
         name: "drum4_charley",
-        color: "E5A90E",
+        color: "ffc075",
         uniqsnd: !0
     }, {
         name: "drum5_chatom",
-        color: "E5A90E",
+        color: "ffc075",
         uniqsnd: !1
     }, {
         name: "effect1_bass",
-        color: "0CB6CB",
+        color: "09b7c8",
         uniqsnd: !1
     }, {
         name: "effect2_enigmatic",
-        color: "0CB6CB",
+        color: "09b7c8",
         uniqsnd: !0
     }, {
         name: "effect3_cry",
-        color: "0CB6CB",
+        color: "09b7c8",
         uniqsnd: !0
     }, {
         name: "effect4_odoyouno",
-        color: "0CB6CB",
+        color: "09b7c8",
         uniqsnd: !0
     }, {
         name: "effect5_oua",
-        color: "0CB6CB",
+        color: "09b7c8",
         uniqsnd: !0
     }, {
         name: "melo1_toun",
-        color: "F42828",
+        color: "e9434d",
         uniqsnd: !1
     }, {
         name: "melo2_flute",
-        color: "F42828",
+        color: "e9434d",
         uniqsnd: !1
     }, {
         name: "melo3_neou",
-        color: "F42828",
+        color: "e9434d",
         uniqsnd: !1
     }, {
         name: "melo4_hu",
-        color: "F42828",
+        color: "e9434d",
         uniqsnd: !0
     }, {
         name: "melo5_ah",
-        color: "F42828",
+        color: "e9434d",
         uniqsnd: !0
     }, { /* MIX FOR PEACE */
         name: "chipsdel",
-        color: "787DFF",
+        color: "7a7ff6",
         uniqsnd: !0
     }, {
         name: "chips2_chillin",
-        color: "787DFF",
+        color: "7a7ff6",
         uniqsnd: !0
     }, {
         name: "chips3_yeah",
-        color: "787DFF",
+        color: "7a7ff6",
         uniqsnd: !0
     }, {
         name: "chips4_filback",
-        color: "787DFF",
+        color: "7a7ff6",
         uniqsnd: !1
     }, {
         name: "chips5_teylo",
-        color: "787DFF",
+        color: "7a7ff6",
         uniqsnd: !0
     }], this.bonusarray = [{ /* BONUS 1 DEMO! */
         name: "Mix For Peace",
