255f7e62389e666b6be798c22e93fcadfde27a95
RizsimStudios <61947503+RizsimStudiosOfficial@users.noreply.github.com>
2024-12-15 17:20:05 -0800

    the original app js

--- asset-v5/app.js
+++ asset-v5/app.js
@@ -1,2 +1,86 @@
-/* 0.5.7 2021-12-27 16:37:09 */
-var app=new function(){this.name="Brazil",this.version="5",this.date="2016",this.folder="asset-v5/",this.looptime=8e3,this.bpm=120,this.totalframe=384,this.nbpolo=7,this.nbloopbonus=1,this.bonusloopA=!0,this.bonusendloopA=!1,this.recmaxloop=23,this.recminloop=3,this.recmintime=Math.round(this.looptime/1e3)*this.recminloop,this.spritepolo="polo-sprite.png",this.spritepolotshirt="polo-sprite-tshirt.png",this.spritepicto="game-picto.png",this.colBck="#0a190f",this.col0="#6EB950",this.col1="#41A032",this.col2="#2D7D28",this.col3="#235023",this.col4="#14321E",this.animearray=[{name:"1_poum",color:"FAB405",uniqsnd:!0},{name:"2_creuki",color:"FAB405",uniqsnd:!0},{name:"3_shaka",color:"FAB405",uniqsnd:!0},{name:"4_chouk",color:"FAB405",uniqsnd:!0},{name:"5_kaliak",color:"FAB405",uniqsnd:!0},{name:"6_tek",color:"23BEE1",uniqsnd:!0},{name:"7_tuk",color:"23BEE1",uniqsnd:!0},{name:"8_teung",color:"23BEE1",uniqsnd:!0},{name:"9_ting",color:"23BEE1",uniqsnd:!0},{name:"10_kougou",color:"23BEE1",uniqsnd:!0},{name:"11_parimba",color:"64B92D",uniqsnd:!0},{name:"12_coloko",color:"64B92D",uniqsnd:!0},{name:"13_clav",color:"64B92D",uniqsnd:!0},{name:"14_tromp",color:"64B92D",uniqsnd:!0},{name:"15_trompolo",color:"64B92D",uniqsnd:!0},{name:"16_bass",color:"E63C14",uniqsnd:!0},{name:"17_wa",color:"E63C14",uniqsnd:!0},{name:"18_ya",color:"E63C14",uniqsnd:!0},{name:"19_palapa",color:"E63C14",uniqsnd:!0},{name:"20_oh",color:"E63C14",uniqsnd:!0}],this.bonusarray=[{name:"Felicidade",src:"v5-b1-felicidade-hb-hd.webm",code:"1,3,6,10,16",sound:"bonus-felicidade",aspire:"aspire"},{name:"Chegou",src:"v5-b2-chegou-hb-hd.webm",code:"1,4,9,13,17",sound:"bonus-chegou",aspire:"aspire"},{name:"Musica",src:"v5-b3-musica-hb-hd.webm",code:"1,5,7,14,19",sound:"bonus-musica",aspire:"aspire"}]};
+/* haha the og thing idfk anymore */
+var app = new function() {
+    this.name = "The Original", this.version = "1", this.date = "2009", this.folder = "asset-v5/", this.looptime = 5333, this.bpm = 90, this.totalframe = 260, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !1, this.bonusendloopA = !1, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#121212", this.col0 = "#919191", this.col1 = "#5f5f5f", this.col2 = "#4b4b4b", this.col3 = "#373737", this.col4 = "#232323", this.animearray = [{
+        name: "percussions1",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "percussions2",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "percussions3",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "effects1",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "effects2",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "effects3",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "effects4",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "8_trill",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "instruments1",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "instruments2",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "instruments3",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "chorus1",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "chorus2",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "voices1",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "voices2", /* tell me the right knee */
+        color: "828282",
+        uniqsnd: !0
+    }], this.bonusarray = [{
+        name: "Choir (OG)",
+        src: "b1-choir.mp4",
+        code: "4,6,7,8,12",
+        sound: "bonus-choir",
+        aspire: "aspire"
+    }, {
+        name: "Sailor (OG)",
+        src: "b2-sailor.mp4",
+        code: "1,8,10,11,14",
+        sound: "bonus-sailor",
+        aspire: "aspire"
+    }, {
+        name: "Santa (OG)",
+        src: "b3-santa.mp4",
+        code: "1,9,12,13,14",
+        sound: "bonus-santa",
+        aspire: "aspire"
+    }];
+    for (var n = 0, o = this.animearray.length; n < o; n++) {
+        var a = this.animearray[n].name;
+        this.animearray[n].soundA = a + "_a", this.animearray[n].soundB = this.animearray[n].uniqsnd ? a + "_a" : a + "_b", this.animearray[n].anime = a + "-sprite.png", this.animearray[n].animeData = a + ".json"
+    }
+};
