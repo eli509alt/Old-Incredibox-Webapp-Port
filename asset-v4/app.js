8b6ee60239f29e29b3ab10db458c1a7887a665ba
RizsimStudios <61947503+RizsimStudiosOfficial@users.noreply.github.com>
2024-12-15 16:20:51 -0800

    mix for peace app js real

--- asset-v4/app.js
+++ asset-v4/app.js
@@ -1,6 +1,6 @@
-/* 0.5.4 2021-04-23 21:06:13 */
+/* MIX FOR PEACE IS BACK! - Rizsim Studios */
 var app = new function() {
-    this.name = "The Love", this.version = "4", this.date = "2014", this.folder = "asset-v4/", this.looptime = 8e3, this.bpm = 120, this.totalframe = 384, this.nbpolo = 7, this.nbloopbonus = 1, this.bonusloopA = !0, this.bonusendloopA = !1, this.recmaxloop = 23, this.recminloop = 3, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#211101", this.col0 = "#D8A365", this.col1 = "#BA7B3D", this.col2 = "#A16224", this.col3 = "#6A3501", this.col4 = "#422101", this.animearray = [{
+    this.name = "Axe Boat", this.version = "4", this.date = "2014", this.folder = "asset-v4/", this.looptime = 8e3, this.bpm = 120, this.totalframe = 384, this.nbpolo = 7, this.nbloopbonus = 1, this.bonusloopA = !0, this.bonusendloopA = !1, this.recmaxloop = 23, this.recminloop = 3, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#211101", this.col0 = "#D8A365", this.col1 = "#BA7B3D", this.col2 = "#A16224", this.col3 = "#6A3501", this.col4 = "#422101", this.animearray = [{
         name: "drum1_kick",
         color: "E5A90E",
         uniqsnd: !1
@@ -60,7 +60,7 @@ var app = new function() {
         name: "melo5_ah",
         color: "F42828",
         uniqsnd: !0
-    }, {
+    }, { /* MIX FOR PEACE */
         name: "chipsdel",
         color: "787DFF",
         uniqsnd: !0
@@ -80,7 +80,7 @@ var app = new function() {
         name: "chips5_teylo",
         color: "787DFF",
         uniqsnd: !0
-    }], this.bonusarray = [{
+    }], this.bonusarray = [{ /* BONUS 1 DEMO! */
         name: "Mix For Peace",
         src: "b1-love.mp4",
         code: "1,2,6,11,17",
