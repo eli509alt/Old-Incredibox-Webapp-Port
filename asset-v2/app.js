131a65b482c74ab136c8cc35d7d6f0292ad76f59
RizsimStudios <61947503+RizsimStudiosOfficial@users.noreply.github.com>
2024-12-19 19:41:39 -0800

    fixing this format to better

--- asset-v2/app.js
+++ asset-v2/app.js
@@ -1,2 +1,102 @@
 /* 0.5.7 2021-12-27 16:37:09 */
-var app=new function(){this.name="Little Miss",this.version="2",this.date="2012",this.folder="asset-v2/",this.looptime=4800,this.bpm=100,this.totalframe=232,this.nbpolo=7,this.nbloopbonus=4,this.bonusloopA=!0,this.bonusendloopA=!0,this.recmaxloop=38,this.recminloop=5,this.recmintime=Math.round(this.looptime/1e3)*this.recminloop,this.spritepolo="polo-sprite.png",this.spritepolotshirt="polo-sprite-tshirt.png",this.spritepicto="game-picto.png",this.colBck="#1c0d0d",this.col0="#EB4B4B",this.col1="#C32D2D",this.col2="#9B2323",this.col3="#691E1E",this.col4="#371919",this.animearray=[{name:"beat1_boom",color:"007c00",uniqsnd:!1},{name:"beat2_kashi",color:"389b14",uniqsnd:!1},{name:"beat3_paomeu",color:"50aa00",uniqsnd:!0},{name:"beat4_ptttpeu",color:"78be00",uniqsnd:!1},{name:"beat5_slupttt",color:"a6d511",uniqsnd:!0},{name:"effet1_poulll",color:"ff0000",uniqsnd:!1},{name:"effet2_tucati",color:"ff2800",uniqsnd:!0},{name:"effet3_tuilopta",color:"ff5609",uniqsnd:!1},{name:"effet4_tululou",color:"ff7800",uniqsnd:!0},{name:"effet5_tumttt",color:"ff7800",uniqsnd:!1},{name:"melo1_nananana",color:"1478aa",uniqsnd:!1},{name:"melo2_pelulu",color:"288cbe",uniqsnd:!1},{name:"melo3_siffle",color:"3a9bcc",uniqsnd:!1},{name:"melo4_tatouti",color:"50b4e6",uniqsnd:!1},{name:"melo5_tvutvutvu",color:"64c8fa",uniqsnd:!1},{name:"coeur1_oaaah",color:"7e0070",uniqsnd:!1},{name:"coeur2_cougou",color:"992794",uniqsnd:!1},{name:"coeur3_porticoeur",color:"a24aa7",uniqsnd:!1},{name:"voix1_isit",color:"d7286e",uniqsnd:!1},{name:"voix2_uare",color:"ff5096",uniqsnd:!1}],this.bonusarray=[{name:"Satisfied",src:"v2-b1-satisfy-hb-hd.webm",code:"1,4,6,9,12",sound:"bonus-satisfy",aspire:"aspire"},{name:"Little Miss",src:"v2-b2-miss-hb-hd.webm",code:"1,2,10,13,15",sound:"bonus-miss",aspire:"aspire"},{name:"Why this world",src:"v2-b3-world-hb-hd.webm",code:"1,3,4,14,18",sound:"bonus-world",aspire:"aspire"}]};
+var app = new function() {
+    this.name = "Little Miss", this.version = "2", this.date = "2012", this.folder = "asset-v2/", this.looptime = 4800, this.bpm = 100, this.totalframe = 232, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepolotshirt = "polo-sprite-tshirt.png", this.spritepicto = "game-picto.png", this.colBck = "#1c0d0d", this.col0 = "#EB4B4B", this.col1 = "#C32D2D", this.col2 = "#9B2323", this.col3 = "#691E1E", this.col4 = "#371919", this.animearray = [{
+        name: "beat1_boom",
+        color: "007c00",
+        uniqsnd: !1
+    }, {
+        name: "beat2_kashi",
+        color: "389b14",
+        uniqsnd: !1
+    }, {
+        name: "beat3_paomeu",
+        color: "50aa00",
+        uniqsnd: !0
+    }, {
+        name: "beat4_ptttpeu",
+        color: "78be00",
+        uniqsnd: !1
+    }, {
+        name: "beat5_slupttt",
+        color: "a6d511",
+        uniqsnd: !0
+    }, {
+        name: "effet1_poulll",
+        color: "ff0000",
+        uniqsnd: !1
+    }, {
+        name: "effet2_tucati",
+        color: "ff2800",
+        uniqsnd: !0
+    }, {
+        name: "effet3_tuilopta",
+        color: "ff5609",
+        uniqsnd: !1
+    }, {
+        name: "effet4_tululou",
+        color: "ff7800",
+        uniqsnd: !0
+    }, {
+        name: "effet5_tumttt",
+        color: "ff7800",
+        uniqsnd: !1
+    }, {
+        name: "melo1_nananana",
+        color: "1478aa",
+        uniqsnd: !1
+    }, {
+        name: "melo2_pelulu",
+        color: "288cbe",
+        uniqsnd: !1
+    }, {
+        name: "melo3_siffle",
+        color: "3a9bcc",
+        uniqsnd: !1
+    }, {
+        name: "melo4_tatouti",
+        color: "50b4e6",
+        uniqsnd: !1
+    }, {
+        name: "melo5_tvutvutvu",
+        color: "64c8fa",
+        uniqsnd: !1
+    }, {
+        name: "coeur1_oaaah",
+        color: "7e0070",
+        uniqsnd: !1
+    }, {
+        name: "coeur2_cougou",
+        color: "992794",
+        uniqsnd: !1
+    }, {
+        name: "coeur3_porticoeur",
+        color: "a24aa7",
+        uniqsnd: !1
+    }, {
+        name: "voix1_isit",
+        color: "d7286e",
+        uniqsnd: !1
+    }, {
+        name: "voix2_uare",
+        color: "ff5096",
+        uniqsnd: !1
+    }], this.bonusarray = [{
+        name: "Satisfied",
+        src: "v2-b1-satisfy-hb-hd.webm",
+        code: "1,4,6,9,12",
+        sound: "bonus-satisfy",
+        aspire: "aspire"
+    }, {
+        name: "Little Miss",
+        src: "v2-b2-miss-hb-hd.webm",
+        code: "1,2,10,13,15",
+        sound: "bonus-miss",
+        aspire: "aspire"
+    }, {
+        name: "Why this world",
+        src: "v2-b3-world-hb-hd.webm",
+        code: "1,3,4,14,18",
+        sound: "bonus-world",
+        aspire: "aspire"
+    }]
+};
