13467146ed802bd54d8001fc1199e7258a7bb7a4
RizsimStudios <61947503+RizsimStudiosOfficial@users.noreply.github.com>
2025-01-06 16:38:03 -0800

    color changing

--- asset-v2/app.js
+++ asset-v2/app.js
@@ -1,6 +1,6 @@
 /* 0.5.7 2021-12-27 16:37:09 */
 var app = new function() {
-    this.name = "Little Miss", this.version = "2", this.date = "2012", this.folder = "asset-v2/", this.looptime = 4800, this.bpm = 100, this.totalframe = 232, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepolotshirt = "polo-sprite-tshirt.png", this.spritepicto = "game-picto.png", this.colBck = "#1c0d0d", this.col0 = "#EB4B4B", this.col1 = "#C32D2D", this.col2 = "#9B2323", this.col3 = "#691E1E", this.col4 = "#371919", this.animearray = [{
+    this.name = "Little Miss", this.version = "2", this.date = "2012", this.folder = "asset-v2/", this.looptime = 4800, this.bpm = 100, this.totalframe = 232, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepolotshirt = "polo-sprite-tshirt.png", this.spritepicto = "game-picto.png", this.colBck = "#1c0d0d", this.col0 = "#17ed13", this.col1 = "#00ff44", this.col2 = "#0c943b", this.col3 = "#0e5c24", this.col4 = "#188c12", this.animearray = [{
         name: "beat1_boom",
         color: "007c00",
         uniqsnd: !1
